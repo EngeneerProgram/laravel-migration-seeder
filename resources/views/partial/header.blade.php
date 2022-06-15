962b02251b3be99bcef02b907f87fa0027f8b537
Rosario Garufi <saro.garufi1@gmail.com>
2022-06-15 16:09:23 +0200

    final commit

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -11,21 +11,21 @@
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#">abaut Us</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Link
+                  Contacts
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Contact</a></li>
+                  <li><a class="dropdown-item" href="#">Gallery</a></li>
                   <li><hr class="dropdown-divider"></li>
                   <li><a class="dropdown-item" href="#">Something else here</a></li>
                 </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link disabled">Link</a>
+                <a class="nav-link ">Gallery</a>
               </li>
             </ul>
             <form class="d-flex" role="search">
--- resources/views/vacanze.blade.php
+++ resources/views/vacanze.blade.php
@@ -10,15 +10,26 @@
 
 </div>
 
-<h1 class="pt-5 cursiv text-center">Scegli il giusto posto per te</h1>
+
+
+
+<div class="container-fluid content_card">
 <div class="pt-5 container">
     <div class="row row-cols-sm-2 row-cols-md-4">
         @foreach($places as $place)
-        <div class="col d-flex flex-column">
+        <div class="col  d-flex flex-column">
             
-            <div class="card">
+            <div class="card ">
                 <div class="img">
                     <img class="img-fluid img_card" src="{{$place->img}}"  alt="">
+                </div>
+                <div class="text_section">
+                    <h3 class="pt-2 text-center">{{$place->destinazione}}</h3>
+                    <h6 >Hotel: {{$place->hotel}}</h6>
+                    <p><strong class="red">Intrattenimento</strong> <br>  {{$place->intrattenimento}}</p>
+                    <hr>
+                    <h6 class="text-center">Storia</h6>
+                    <p class="text-center">{{$place->descrizione}}</p>
                 </div>
             </div>
 
@@ -26,5 +37,7 @@
         </div>
         @endforeach
     </div>
+</div>
+
 </div>
 @endsection
